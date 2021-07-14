c8af157e100d4aed25ae201314fb5b252d44117a
Benjamin Day <benday@benday.com>
2021-07-14 07:07:45 -0400

    got basic integration test working

--- test/Benday.SeleniumDemo.IntegrationTests/IntegrationTestFixtures.cs
+++ test/Benday.SeleniumDemo.IntegrationTests/IntegrationTestFixtures.cs
@@ -1,4 +1,7 @@
+using Benday.SeleniumDemo.WebUi;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
 
 namespace Benday.SeleniumDemo.IntegrationTests
 {
@@ -6,8 +9,25 @@ namespace Benday.SeleniumDemo.IntegrationTests
     public class IntegrationTestFixtures
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task CallHomePage_UsingWebApplicationFactory()
         {
+            var factory = new WebApplicationFactory<Startup>();
+
+            var expectedText = "text that should always be there";
+
+            var client = factory.CreateClient();
+
+            var url = "home/index";
+
+            var response = await client.GetAsync(url);
+
+            Assert.IsTrue(response.IsSuccessStatusCode, "Request should have succeeded");
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            Assert.IsNotNull(content, "content was null");
+
+            Assert.IsTrue(content.Contains(expectedText), "content should contain expected text");
         }
     }
 }
