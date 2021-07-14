ee21e17522f0456f868a4a4589c4b3247e5ea506
Benjamin Day <benday@benday.com>
2021-07-14 07:30:17 -0400

    Well, i'm getting somewhere...but it still doesn't work

--- test/Benday.SeleniumDemo.IntegrationTests/IntegrationTestFixtures.cs
+++ test/Benday.SeleniumDemo.IntegrationTests/IntegrationTestFixtures.cs
@@ -1,6 +1,12 @@
 using Benday.SeleniumDemo.WebUi;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Edge.SeleniumTools;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Benday.SeleniumDemo.IntegrationTests
@@ -8,14 +14,32 @@ namespace Benday.SeleniumDemo.IntegrationTests
     [TestClass]
     public class IntegrationTestFixtures
     {
+        [TestInitialize]
+        public void OnTestInitialize()
+        {
+            _SystemUnderTest = null;
+        }
+
+        private WebApplicationFactory<Startup> _SystemUnderTest;
+        public WebApplicationFactory<Startup> SystemUnderTest
+        {
+            get
+            {
+                if (_SystemUnderTest == null)
+                {
+                    _SystemUnderTest = new WebApplicationFactory<Startup>();
+                }
+
+                return _SystemUnderTest;
+            }
+        }
+
         [TestMethod]
         public async Task CallHomePage_UsingWebApplicationFactory()
         {
-            var factory = new WebApplicationFactory<Startup>();
-
             var expectedText = "text that should always be there";
 
-            var client = factory.CreateClient();
+            var client = SystemUnderTest.CreateClient();
 
             var url = "home/index";
 
@@ -28,6 +52,48 @@ namespace Benday.SeleniumDemo.IntegrationTests
             Assert.IsNotNull(content, "content was null");
 
             Assert.IsTrue(content.Contains(expectedText), "content should contain expected text");
+        }
+        
+        [TestMethod]
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public async Task CallHomePage_UsingSeleniumAndWebApplicationFactory()
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {    
+            var expectedText = "text that should always be there";
+
+            var client = SystemUnderTest.CreateClient();
+
+            var url = "home/index";
+            var fullyQualifiedUrl = GetFullUrl(url);
+
+            var driverOptions = new EdgeOptions();
+            driverOptions.UseChromium = true;
+            
+            // if there is an issue with the run in CI, comment the headless part
+            // driverOptions.AddArguments("--headless", "--ignore-certificate-errors");
+
+            // using var driver = new EdgeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), driverOptions);
+            using var driver = new EdgeDriver(driverOptions);
+
+            // act
+            driver.Navigate().GoToUrl(fullyQualifiedUrl);
+
+            // assert
+            var divThatShouldExistAlways = driver.FindElement(By.Id("divTextThatIsAlwaysThere"));
+
+            Assert.IsNotNull(divThatShouldExistAlways, "div should not be null");
+            Assert.IsTrue(divThatShouldExistAlways.Displayed, "div should be displayed");
+            Assert.IsTrue(divThatShouldExistAlways.Enabled, "div should be enabled");
+
+            Assert.IsTrue(divThatShouldExistAlways.Text.Contains(expectedText), "div should contain expected text");            
+        }
+
+        private string GetFullUrl(string url)
+        {
+            var baseAddr = SystemUnderTest.Server.BaseAddress;
+            var uri = new Uri(baseAddr, url);
+
+            return uri.AbsoluteUri;
         }
     }
 }
