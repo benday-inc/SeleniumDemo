8178fcb6bde3ee4ed3c7ad30521e5b2b5a1ad67f
Benjamin Day <benday@benday.com>
2021-07-14 06:58:11 -0400

    renamed integration test file

---
+++ test/Benday.SeleniumDemo.IntegrationTests/IntegrationTestFixtures.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Benday.SeleniumDemo.IntegrationTests
+{
+    [TestClass]
+    public class IntegrationTestFixtures
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
